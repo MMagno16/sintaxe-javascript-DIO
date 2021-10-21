1e36507c8c88a0f30d48ce524ffe3dd00099d86e
Marianna Magno <89035572+MMagno16@users.noreply.github.com>
2021-10-21 14:12:47 -0300

    Add files via upload

---
+++ aula2/sintaxe-em-javascriptdioaula2variaveis/src/script.js
@@ -0,0 +1,94 @@
+//tipos primitivos
+
+//boolean
+/*var vOUf = false;
+console.log(typeof(vOUf));
+
+//number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+//string
+var nome = 'diana';
+console.log(typeof(nome));*/
+
+//como declarar
+/*var variavel = 'diana';
+variavel = 'martine';
+console.log('variavel');*/
+
+/*let variavel2 = 'diana';
+variavel2 = 'martine';
+console.log(variavel2);*/
+
+/*const constante = 'diana';
+console.log(constante);*/
+
+/*var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal() {
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno);
+}
+escopoLocal();*/
+
+/*/atribuição
+var atribuição = 'diana';
+
+//comparação
+var comparação = '0' == 0;
+console.log(comparação);
+
+//comparação identica
+var comparacaoidentica = '0' == 0;
+console.log(comparacaoidentica);*/
+
+//adição
+/*var adicao = 1 + 1;
+console.log(adicao);*/
+
+//subtração
+/*var subtracao = 2 - 1;
+console.log(subtracao);*/
+
+//mutiplicação
+/*var multiplicacao = 2 * 3;
+console.log(multiplicacao);*/
+
+//divisão real
+/*var divisaoReal = 6 / 2;
+console.log(divisaoReal);*/
+
+//divisao inteira
+/*var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);*/
+
+//potenciação
+/*var potenciacao = 2 ** 10;
+console.log(potenciacao);*/
+
+//maior que
+/*var maiorQue = 5 > 2;
+console.log(maiorQue);*/
+
+//menor que
+/*var menorQue = 5 < 2;
+console.log(menorQue);*/
+
+//maior ou igual a
+/*var maiorOUIgual = 5 >= 2;
+console.log(maiorOUIgual);*/
+
+//menor ou igual a
+/*var menorOUIgual = 5 <= 2;
+console.log(menorOUIgual);*/
+
+/*var e = true && false;
+console.log(e);*/
+
+/*var ou = true || false;
+console.log(ou);*/
+
+/*var nao = !true;
+console.log(nao);*/
---
+++ aula6/sintaxe-em-javascriptdio-aula6tipos-de-fun-es/src/script.js
@@ -0,0 +1,19 @@
+//tipos de função
+//declarativas
+/*function funcao(){
+    console.log('Tudo certo jovem!')
+}
+funcao();*/
+
+//expressões de funções
+//com nomeaçao
+/*var funcao = function fun(){
+    console.log('Sou uma mensagem de função de expressão')
+}
+funcao();*/
+
+//arrow function
+/*var funcao = () =>{
+    console.log('Sou uma arrow function!')
+}
+funcao();*/
